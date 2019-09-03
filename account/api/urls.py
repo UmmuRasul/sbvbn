bdcd701cf4b9223698ebba94a0ac23bc6f0b3fdf
Ummu Rasul <ummurasul5@gmail.com>
2019-09-03 09:33:44 +0300

    user registration

---
+++ account/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from account.api.views import (
+    registartion_view,
+)
+
+
+app_name = 'account'
+urlpatterns = [
+    path('register', registartion_view, name='register')
+]
